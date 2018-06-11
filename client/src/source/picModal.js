88e5ae8b642857e8c935a3abc730fa4a8b6247f4
黄伟 <huangwei10@jd.com>
2018-06-11 19:18:23 +0800

    commit

--- client/src/source/picModal.js
+++ client/src/source/picModal.js
@@ -1,15 +1,42 @@
-$("body").on('click', ".imgsWrap img", function () {
-    //alert($(this).attr('src'));
-    $('body').append("<div class='mb-wrapper imgShow'><img src='" + $(this).attr('src') + "'/></div>");
-    window.setTimeout(function () {
-        $('.imgShow>img').addClass('show');
-    }, 50);
-
-    $(".imgShow").one("click", function () {
-        var _self = this;
-        $(this).find("img").removeClass('show');
+$(function () {
+    if (!wx) {
+        $('body').append("<div class='mb-wrapper imgShow'><img src='" + $(this).attr('src') + "'/></div>");
         window.setTimeout(function () {
-            $(_self).remove();
-        }, 300);
-    });
+            $('.imgShow>img').addClass('show');
+        }, 50);
+        $(".imgShow").one("click", function () {
+            var _self = this;
+            $(this).find("img").removeClass('show');
+            window.setTimeout(function () {
+                $(_self).remove();
+            }, 300);
+
+        });
+    } else {
+        // $("body").on('click', ".imgsWrap img", function () {
+        //     //alert($(this).attr('src'));
+        //     var curImageSrc = $(this).attr('src');
+        //     var imgArray = [];
+        //     $('.imgsWrap img').each(function (index, el) {
+        //         var itemSrc = $(this).attr('src');
+        //         imgArray.push(itemSrc);
+        //     });
+        //     wx.previewImage({
+        //         current: curImageSrc,
+        //         urls: imgArray
+        //     });
+        // });
+        var imgArray = []
+        $.each($('.imgsWrap img'), function (index, el) {
+            if (el.src) {
+                imgArray.push(el.src);
+                $(el).click(function () {
+                    wx.previewImage({
+                        current: this.src,
+                        urls: imgArray
+                    });
+                })
+            }
+        })
+    }
 })
